38b0b356da08aa121cd7671b284fda56ca213317
jessicah91 <jes_sicah@naver.com>
2026-04-02 18:12:03 +0900

    Delete api directory

--- api/source-debug.js
+++
@@ -1,7 +0,0 @@
-const { fetchDailyStandings, fetchScoreboardForDate } = require('./_lib/kbo');
-module.exports = async (req, res) => {
-  const date = String(req.query.date || new Date().toISOString().slice(0, 10));
-  const standings = await fetchDailyStandings(true).catch((e) => ({ error: e.message }));
-  const scoreboard = await fetchScoreboardForDate(date, true).catch((e) => ({ error: e.message }));
-  res.status(200).json({ date, standings, scoreboard });
-};
